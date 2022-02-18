b404b0a4152a3f27d2e20c7284db2b1bf850d4a1
mado <madelineadonley@gmail.com>
2022-02-18 13:02:55 -0700

    updated server and auth files

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas')
+const { authMiddleware } = require('./util/auth')
+
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
+
+console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }

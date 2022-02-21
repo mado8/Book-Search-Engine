372af6eb926b69a3e255c52a239df714d2c3457d
mado <madelineadonley@gmail.com>
2022-02-20 17:38:18 -0700

    finished searchBooks component

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -72,10 +72,6 @@ const SearchBooks = () => {
     try {
       const response = await saveBook({variables: { bookData: bookToSave }});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
